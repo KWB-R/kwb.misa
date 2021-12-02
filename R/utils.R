5a7ab0e1c508269c0670ca3d5ff0b73fa6218e66
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-12-02 09:59:34 +0100

    Move count_repeats() out of same_inarow()
    This allows us e.g. to test it separately.

--- R/utils.R
+++ R/utils.R
@@ -21,24 +21,6 @@ same_inarow <- function(
   v_extended <- as.character(c(v, 0))
   v_extended[is.na(v_extended)] <- NA_treatment
 
-  # supporting function --------------------------------------------------------
-  count_repeats <- function(
-    v, # Vector
-    beg
-  ){
-    n <- 0
-    for(i in beg:length(v)){
-      if(v[i] == v[beg]){
-        n <- n + 1
-      } else {
-        break
-      }
-    }
-    data.frame("Value" = v[beg], "repeats" = n,
-               "starts_at" = beg, "ends_at" = i - 1)
-  }
-  # ----------------------------------------------------------------------------
-
   rep_list <- list()
   beg <- 1
   list_entry <- 1
@@ -51,4 +33,21 @@ same_inarow <- function(
   finalFrame <- do.call(rbind, rep_list)
   finalFrame$Value <- as.character(finalFrame$Value)
   finalFrame
+}
+
+# supporting function --------------------------------------------------------
+count_repeats <- function(
+  v, # Vector
+  beg
+){
+  n <- 0
+  for(i in beg:length(v)){
+    if(v[i] == v[beg]){
+      n <- n + 1
+    } else {
+      break
+    }
+  }
+  data.frame("Value" = v[beg], "repeats" = n,
+             "starts_at" = beg, "ends_at" = i - 1)
 }
