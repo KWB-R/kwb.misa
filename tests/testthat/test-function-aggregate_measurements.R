0976de95d776dc68e5c85cedc723dcb6f2e3f334
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-06-14 16:30:10 +0200

    Use expect_equal, test microbenchmark

--- tests/testthat/test-function-aggregate_measurements.R
+++ tests/testthat/test-function-aggregate_measurements.R
@@ -66,24 +66,30 @@ test_that("aggregate_measurements() works", {
   expected3_60 <- expected2_60
   expected3_120 <- expected2_120
 
-  # Function to check for equality, not identity
-  check <- function(data, expected) expect_true(all.equal(data, expected))
+  # install.packages("microbenchmark")
+  # microbenchmark::microbenchmark(
+  #   f1(data1$t, data1$d, 60),
+  #   f2(data1$t, data1$d, 60),
+  #   times = 1000,
+  #   check = "identical"
+  #   #check = "equal"
+  # )
 
   # checking data1, 60s
-  check(f1(data1$t, data1$d, 60), expected1_60)
-  check(f2(data1$t, data1$d, 60), expected1_60)
+  expect_equal(f1(data1$t, data1$d, 60), expected1_60)
+  expect_equal(f2(data1$t, data1$d, 60), expected1_60)
 
   # checking data2, 60s
-  check(f1(data2$t, data2$d, 60), expected2_60)
-  check(f2(data2$t, data2$d, 60), expected2_60)
+  expect_equal(f1(data2$t, data2$d, 60), expected2_60)
+  expect_equal(f2(data2$t, data2$d, 60), expected2_60)
 
   # checking data3, 60s
-  #check(f1(data3$t, data3$d, 60), expected3_60) # fails!
-  check(f2(data3$t, data3$d, 60), expected3_60)
+  #expect_equal(f1(data3$t, data3$d, 60), expected3_60) # fails!
+  expect_equal(f2(data3$t, data3$d, 60), expected3_60)
 
   # checking data1, 120s
-  #check(f1(data1$t, data1$d, 120), expected1_120) # fails!
-  check(f2(data1$t, data1$d, 120), expected1_120)
+  #expect_equal(f1(data1$t, data1$d, 120), expected1_120) # fails!
+  expect_equal(f2(data1$t, data1$d, 120), expected1_120)
 
   # checking data2, 120s
   #check(f1(data2$t, data2$d, 120), expected2_120) # fails!
