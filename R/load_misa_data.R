27556574eb2130229178c5eb48538ba796c55fbf
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2022-09-23 13:27:35 +0200

    Move functions that load outlet data from interface function .R file to new, separate .R file

---
+++ R/load_misa_data.R
@@ -0,0 +1,109 @@
+#' Loads the parameter ID table
+#'
+#' @details
+#' Columns:
+#' parameter: full German Parameter Name
+#' id_infoworks: parameter ID in Infoworks (mf = mass flow)
+#' unit_infoworks: unit of the infoworks output (mass flow)
+#' constant_value: parameters that are not simulated by infoworks and are set
+#' to a constant value
+#' unit is the same as in gerris (see column "unit_gerris")
+#' id_gerris: parameter ID in Gerris
+#' unit_gerris: unit needed for Gerris input (concentration)
+#' min_gerris and max_gerris: tolerated range of parameter in Gerris model
+#' comment: possibility to add comments (column not used by r-script)
+#' @importFrom readxl read_excel
+#' @export
+#'
+paraIDs <- function(){
+  as.data.frame(
+    readxl::read_excel(
+      path = system.file(
+        "extdata/interface/parameter_conversion.xlsx",
+        package = "kwb.misa"
+      )
+    )
+  )
+}
+
+#' Loads the package outlet ID table
+#'
+#' @details
+#' Columns:
+#' outlet_id: Node ID of the outlet within the infoworks model
+#' upstream_link_id: Infoworks ID of the link leading to outlet (this must be
+#' usedfor mass flow information)
+#' gerris_id: "Randbedingung" used in gerris (provided by Schumacher,
+#' Ingenieurbüro für Wasser und Umwelt)
+#' surface_water: name of the surface water the outlet discharges into
+#' water_body_km: the water body km where the outlet is located
+#' y_coordinate and x_coordanite: outlet location in soldner coordinates for
+#' Berlin
+#' all lat and lon columns: coordinates transformed from Soldner to UTM-Format
+#' (ETRS89 DREF91)
+#' comment: possibility to add comments (column not used by r-script)
+#' @importFrom readxl read_excel
+#' @export
+#'
+outletIDs <- function(){
+  as.data.frame(
+    readxl::read_excel(
+      path = system.file(
+        "extdata/interface/outlet_conversion.xlsx",
+        package = "kwb.misa"
+      )
+    )
+  )
+}
+
+#' Loads river sites for comparing bar plots
+#'
+#' @return
+#' Table as defined in 'extdata/misa_data' filtered for all rows
+#' containing "y" in the "Auswahl"-column
+#'
+#' @export
+#'
+loadMisa_focus_sites <- function(){
+  path <- file.path(system.file(package = "kwb.misa"), "extdata/misa_data")
+
+  sid <- read.table(
+    file = file.path(path, "focus_sites.csv"), header = T,sep = ";", dec = ".")
+
+  sid[sid$use == "y",]
+}
+
+#' Loads critical rain events for scenario assessment
+#'
+#' @return
+#' Table as defined in 'extdata/misa_data' filtered
+#'
+#' @export
+#'
+loadMisa_events <- function(){
+  path <- file.path(system.file(package = "kwb.misa"), "extdata/misa_data")
+
+  e_data <- read.table(file = file.path(path,"ereignisreihe.csv"),
+                       header = T, sep = ";", dec = ".")
+
+  e_data$tBeg <- as.POSIXct(e_data$tBeg, format = "%d.%m.%Y %H:%M")
+  e_data$tEnd <- as.POSIXct(e_data$tEnd, format = "%d.%m.%Y %H:%M")
+
+  e_data
+}
+
+#' Loads table with decoupling information
+#'
+#' @return
+#' Table as defined in 'extdata/misa_data'
+#'
+#' @export
+#'
+loadMisa_decouplingInfo <- function(){
+  path <- file.path(system.file(package = "kwb.misa"), "extdata/misa_data")
+  read.table(
+    file = file.path(path, "outlet_decoupling.csv"),
+    header = T,sep = ";", dec = ".")
+}
+
+
