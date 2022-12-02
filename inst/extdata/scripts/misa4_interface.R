689c874cd9cf679b379bc2050de39e6b236c7de5
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2022-12-02 16:22:07 +0100

    Add script for MiSa4-Interface. New folder paths, less description that is not needed

---
+++ inst/extdata/scripts/misa4_interface.R
@@ -0,0 +1,74 @@
+
+
+szenario_id <- "basis"
+
+
+if(FALSE){
+  # Wo liegen die Dateien von Infoworks?
+  interface_input_folder <- file.path(
+    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/berechnungen",
+    szenario_id, "1_iw_output")
+
+  # Wo sollen die für ormatierten Dateien abgespeichert werden?
+  interface_output_folder <- file.path(
+    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/berechnungen",
+    szenario_id, "2_interface_output")
+
+  # Simulationsname (hier reicht ein eindeutiger Teil des Namens) und
+  # Vorlauftrockenzeit
+  simulations <- c(
+    "E2_2011" = 5, "E3_2011" = 5, "E5_2012" = 5, "E6_2012" = 5, "E7_2013" = 22,
+    "E8_2013" = 5, "E9_2014" = 5, "E10_2014" = 6, "E12_2015" = 5,"E13_2016" = 5,
+    "E16_2017" = 6, "E17_2017" = 5, "E19_2018" = 14, "E20_2019" = 5)
+
+  # Falls TRUE, werden Wasserqualitätsparameter nicht mit simuliert
+  flow_only <- FALSE
+
+
+  for(i in seq_along(simulations)){
+    simulation_name <- names(simulations)[i]
+    print(simulation_name)
+    trocken <- simulations[i]
+
+    result <- kwb.misa::iw_gerris_interface(
+      interface_input_folder = interface_input_folder,
+      interface_output_folder = interface_output_folder,
+      simulation_name = simulation_name,
+      return_after = "",
+      return_output_table = TRUE,
+      infoworks_time_format = "%d/%m/%Y %H:%M:%S",
+      gerris_time_format = "%d.%m.%Y %H:%M",
+      timestep_out = 15,
+      flow_only = flow_only,
+      skip_hours = trocken * 24,
+      flow_threshold = 0.003
+    )
+  }
+
+
+  # Additional checks  ###########################################################
+  # Checken, warum einige Werte zu hoch sind
+  result <- iw_gerris_interface(
+    interface_input_folder = interface_input_folder,
+    interface_output_folder = interface_output_folder,
+    simulation_name = simulation_name,
+    stop_after = 5,
+    return_output_table = TRUE,
+    infoworks_time_format = "%d/%m/%Y %H:%M:%S",
+    gerris_time_format = "%d.%m.%Y %H:%M",
+    timestep_out = 15,
+    flow_only = flow_only,
+    skip_hours = trocken * 24,
+    flow_threshold = 0.003)
+
+  # Memo: Die warnungen zur Korrktur der Messwerte werden nicht in die Log-Datei
+  # geschrieben sondern in die Console -> sollte geaendert werden
+
+  check_high_concentration(
+    result = result,
+    parameter_conversion = parameter_conversion,
+    parameterID_infoworks = "mfcodtot"
+  )
+}
+
+
