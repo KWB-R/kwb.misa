8ade4e135f9bd27a03de1ea18ce4ac4f31e01e76
fknoche <franziska.knoche@kompetenz-wasser.de>
2023-06-20 16:02:01 +0200

    - paths were edited

--- inst/extdata/scripts/misa4_interface.R
+++ inst/extdata/scripts/misa4_interface.R
@@ -1,17 +1,17 @@
 
 
-szenario_id <- "basis"
+szenario_id <- "S7"
 
 
 if(FALSE){
   # Wo liegen die Dateien von Infoworks?
   interface_input_folder <- file.path(
-    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/berechnungen",
+    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/AP3_Szenarienrechnung/berechnungen",
     szenario_id, "1_iw_output")
 
   # Wo sollen die für ormatierten Dateien abgespeichert werden?
   interface_output_folder <- file.path(
-    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/berechnungen",
+    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/AP3_Szenarienrechnung/berechnungen",
     szenario_id, "2_interface_output")
 
   # Simulationsname (hier reicht ein eindeutiger Teil des Namens) und
@@ -19,7 +19,8 @@ if(FALSE){
   simulations <- c(
     "E2_2011" = 5, "E3_2011" = 5, "E5_2012" = 5, "E6_2012" = 5, "E7_2013" = 22,
     "E8_2013" = 5, "E9_2014" = 5, "E10_2014" = 6, "E12_2015" = 5,"E13_2016" = 5,
-    "E16_2017" = 6, "E17_2017" = 5, "E19_2018" = 14, "E20_2019" = 5)
+    "E16_2017" = 6, "E17_2017" = 5,
+    "E19_2018" = 14, "E20_2019" = 5)
 
   # Falls TRUE, werden Wasserqualitätsparameter nicht mit simuliert
   flow_only <- FALSE
@@ -47,7 +48,7 @@ if(FALSE){
   }
 
 
-  # Additional checks  ###########################################################
+  # Additional checks  #########################################################
 
 
 
