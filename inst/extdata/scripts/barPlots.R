c400938769ca39e95692da05a0966a375a824f05
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2023-07-06 17:23:31 +0200

    Loop barplots over all focus sites

--- inst/extdata/scripts/barPlots.R
+++ inst/extdata/scripts/barPlots.R
@@ -10,45 +10,54 @@ fs <- kwb.misa::loadMisa_focus_sites(used_sites_only = FALSE)
 df_plot <- kwb.misa::prepareBarplot(
   rdata_files = list(
     file.path(misa4_path, "S0_basis", "5_assessment_output", "misa_tool_basis.RData"),
-    file.path(misa4_path, "S3", "5_assessment_output", "misa_tool_S3.RData")),
+    file.path(misa4_path, "S3", "5_assessment_output", "misa_tool_S3.RData"),
+    file.path(misa4_path, "S4", "5_assessment_output", "misa_tool_S4.RData"),
+    file.path(misa4_path, "S7", "5_assessment_output", "misa_tool_S7.RData")),
   scenario_names = c(
     "basis",
-    "str10_bln7"),
+    "str10_bln7",
+    "lieg10_bln7",
+    "strlieg10_bln7"),
   qsim_focus_sites = fs$QSim_name)
 
 df_plot <- merge(x = df_plot, y = fs, by.x = "qsim_site", by.y = "QSim_name", all = TRUE)
 
 
+
+
 # Eine Messstelle --------------------------------------------------------------
-unique(df_plot$ID)
+for(siteName in unique(df_plot$ID)){
+  png(filename = paste0(misa4_path, "/szenarienvergleich/barplot_", siteName, ".png"),
+      width = 6, height = 10, units = "in", res = 300)
+  {
+    #dev.new(noRStudioGD = TRUE, height = 10, widths = 6)
+    layout(mat = c(1,2,3,4))
 
-siteName <- "SOP"
-{
-  #png(filename = )
-  dev.new(noRStudioGD = TRUE, height = 10, widths = 6)
-  layout(mat = c(1,2,3,4))
-
-  kwb.misa::barPlot_site(
-    df_plot = df_plot,
-    siteName = siteName,
-    barType = "crit_ox"
-  )
-  kwb.misa::barPlot_site(
-    df_plot = df_plot,
-    siteName = siteName,
-    barType = "comf_ox"
-  )
-  kwb.misa::barPlot_site(
-    df_plot = df_plot,
-    siteName = siteName,
-    barType = "crit_events"
-  )
-  kwb.misa::barPlot_site(
-    df_plot = df_plot,
-    siteName = siteName,
-    barType = "neg_dev"
-  )
+    kwb.misa::barPlot_site(
+      df_plot = df_plot,
+      siteName = siteName,
+      barType = "crit_ox"
+    )
+    kwb.misa::barPlot_site(
+      df_plot = df_plot,
+      siteName = siteName,
+      barType = "comf_ox"
+    )
+    kwb.misa::barPlot_site(
+      df_plot = df_plot,
+      siteName = siteName,
+      barType = "crit_events"
+    )
+    kwb.misa::barPlot_site(
+      df_plot = df_plot,
+      siteName = siteName,
+      barType = "neg_dev"
+    )
+  }
+  dev.off()
 }
+
+
 
 
 # Alle Schwerpunkte ------------------------------------------------------------
