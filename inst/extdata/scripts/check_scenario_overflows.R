aa12af317f6d3d0b69933c9cff5f71dc0d5e79a4
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2023-07-12 19:57:50 +0200

    New script for comparing the outlet volumes of two scenarios

---
+++ inst/extdata/scripts/check_scenario_overflows.R
@@ -0,0 +1,88 @@
+library(dplyr)
+if(FALSE){
+  s_a <- stat_summary(scenario = "S0")
+  s_b <- stat_summary(scenario = "S7", include_bsb = TRUE)
+
+  df <- merge(x = s_a, y = s_b, by = "RbId")
+
+  outlet_catch <- kwb.misa:::loadMisa_decouplingInfo()
+
+  df <- merge(x = df, y = outlet_catch, by.x = "RbId", by.y = "gerris_id")
+  # Regarding Volume
+  df2 <- df %>% mutate("diff" = tVol.x - tVol.y,
+                       "removal" = round(diff / tVol.x * 100, 0))
+
+  hist(df2$removal[df2$removal > 0])
+  summary(df2$removal[df2$removal > 0])
+  summary(df2$diff[df2$removal > 0])
+  summary(df2$diff)
+  df3 <- df2 %>% group_by(surface_water.x) %>%
+    summarize("mean_removal" = mean(removal),
+              "mean_absol" = mean(diff))
+
+  df3 <- df2 %>% group_by(catchment_arab) %>%
+    summarize("mean_removal" = mean(removal),
+              "sum_removal" = sum(diff),
+              "n" = n(),
+              "mean_absolut" = sum_removal / n)
+
+  df2 %>% filter(removal > 0) %>%
+    group_by(surface_water.x) %>%
+    summarize("mean_removal" = mean(removal))
+
+  df3 <- df2 %>% filter(removal > 0) %>%
+    group_by(catchment_arab) %>%
+    summarize("mean_removal" = mean(removal))
+
+  # Regarding BSB
+  df2 <- df %>% mutate("diff" = tBSB.x - tBSB.y,
+                       "removal" = round(diff / tBSB.x * 100, 0))
+
+  df2 %>% group_by(surface_water.x) %>%
+    summarize("mean_removal" = mean(removal, na.rm = TRUE),
+              "mean_absolute" = mean(diff, na.rm = TRUE))
+
+  df2 %>% filter(removal > 0) %>%
+    group_by(surface_water.x) %>%
+    summarize("mean_removal" = mean(removal, na.rm = TRUE),
+              "mean_absolute" = mean(diff, na.rm = TRUE) / mean(tBSB.x) * 100)
+}
+
+stat_summary <- function(scenario, include_bsb = TRUE){
+  scenario_path <- file.path(
+    "Y:/AUFTRAEGE/_Auftraege_laufend/MISA4/Data-Work packages/AP3_Szenarienrechnung/berechnungen",
+    scenario
+  )
+
+  #Path of CSO stats from interface
+  statPath <- file.path(scenario_path,  "2_interface_output")
+
+  all_stats <- grep(pattern = "stats.csv$", x = dir(statPath, full.names = TRUE), value = TRUE)
+
+  stat_list <- list()
+  for(i in seq_along(all_stats)){
+    stat_list[[i]] <- read.table(
+      file = all_stats[i], header = TRUE, sep = ";", dec = "."
+    )
+    stat_list[[i]]$event_count <- i
+  }
+
+  df_stat <- do.call(rbind, stat_list)
+
+  if(include_bsb){
+    df_stat %>%
+      group_by(RbId, outlet_id, upstream_link_id, surface_water) %>%
+      summarize("tVol" = sum(tVol_m3, na.rw = TRUE),
+                "tBSB" = sum(tBSB_g, na.rm = TRUE))
+  } else {
+    df_stat %>%
+      group_by(RbId, outlet_id, upstream_link_id, surface_water) %>%
+      summarize("tVol" = sum(tVol_m3, na.rw = TRUE))
+  }
+
+}
+
+
+
+
+
