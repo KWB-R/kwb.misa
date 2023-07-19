134392ac9f89e744643ac158b425476c2f8ac488
ma-z-am <malte.zamzow@kompetenz-wasser.de>
2023-07-19 20:01:47 +0200

    Adjust script to new interface option: Variable "outlets_to_suppress" added

--- inst/extdata/scripts/misa4_interface.R
+++ inst/extdata/scripts/misa4_interface.R
@@ -1,7 +1,8 @@
 
 
-szenario_id <- "S4"
-
+szenario_id <- "S3"
+outlets_to_suppress = NULL
+  #"16234008"
 
 if(FALSE){
   # Wo liegen die Dateien von Infoworks?
@@ -17,7 +18,8 @@ if(FALSE){
   # Simulationsname (hier reicht ein eindeutiger Teil des Namens) und
   # Vorlauftrockenzeit
   simulations <- c(
-    "E2_2011" = 5, "E3_2011" = 5, "E5_2012" = 5, "E6_2012" = 5, "E7_2013" = 22,
+    #"E2_2011" = 5,
+    "E3_2011" = 5, "E5_2012" = 5, "E6_2012" = 5, "E7_2013" = 22,
     "E8_2013" = 5, "E9_2014" = 5, "E10_2014" = 6, "E12_2015" = 5,"E13_2016" = 5,
     "E16_2017" = 6, "E17_2017" = 5,
     "E19_2018" = 14, "E20_2019" = 5)
@@ -26,12 +28,13 @@ if(FALSE){
   flow_only <- FALSE
 
 
+  all_results <- list()
   for(i in seq_along(simulations)){
     simulation_name <- names(simulations)[i]
     print(simulation_name)
     trocken <- simulations[i]
 
-    result <- kwb.misa::iw_gerris_interface(
+    all_results[[i]] <- kwb.misa::iw_gerris_interface(
       interface_input_folder = interface_input_folder,
       interface_output_folder = interface_output_folder,
       simulation_name = simulation_name,
@@ -42,25 +45,14 @@ if(FALSE){
       timestep_out = 15,
       flow_only = flow_only,
       skip_hours = trocken * 24,
-      flow_threshold = 0.003
+      flow_threshold = 0.003,
+      outlets_to_suppress = outlets_to_suppress
     )
     sink()
-
-    overflow_steps <- data.frame(
-      result %>% group_by(RbId, ParamId) %>%
-        summarize(n = n()) %>%
-        filter(ParamId == "Q", n > 2))
-    bp <- boxplot(overflow_steps$n)
-    cat(paste0(
-      "Suspicious long overflows: ",
-      paste0(overflow_steps$RbId[overflow_steps$n %in% bp$out], collapse = ", ")
-    ))
   }
 
 
   # Additional checks  #########################################################
-
-
 
   # Checken, warum einige Werte zu hoch sind
   result <- kwb.misa::iw_gerris_interface(
@@ -76,13 +68,27 @@ if(FALSE){
     skip_hours = trocken * 24,
     flow_threshold = 0.003)
 
-  # Memo: Die warnungen zur Korrktur der Messwerte werden nicht in die Log-Datei
-  # geschrieben sondern in die Console -> sollte geaendert werden
-
   kwb.misa::check_high_concentration(
     result = result2,
     parameterID_infoworks = "mfcodtot"
   )
+
+  # Checken, ob es irgendwo verdächtig langandauernde Überläufe gibt
+  library(dplyr)
+
+  susp <- lapply(all_results, function(df){
+    overflow_steps <- data.frame(
+      df %>% group_by(RbId, ParamId) %>%
+        summarize(n = n()) %>%
+        filter(ParamId == "Q", n > 2))
+    bp <- boxplot(
+      overflow_steps$n,
+      range = 5,
+      plot = FALSE)
+    overflow_steps$RbId[overflow_steps$n %in% bp$out]
+  })
+  sort(summary(as.factor(unlist(susp))), decreasing = TRUE)
+
 }
 
 
